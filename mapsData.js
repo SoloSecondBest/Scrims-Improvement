e086e887e1461c1a2839171044784295fabdc0fd
SoloSecondBest <jokubas.saudargas2020@gmail.com>
2026-08-05 22:34:25 +0300

    Add files via upload

---
+++ mapsData.js
@@ -0,0 +1,28 @@
+const mapDatabase = [
+
+{
+name:"Dry Season",
+mode:"Bounty",
+image:"images/dry_season.png"
+},
+
+{
+name:"Hideout",
+mode:"Bounty",
+image:"images/hideout.png"
+},
+
+{
+name:"Layer Cake",
+mode:"Bounty",
+image:"images/layer_cake.png"
+},
+
+
+{
+name:"Pit Stop",
+mode:"Heist",
+image:"images/pit_stop.png"
+}
+
+];
