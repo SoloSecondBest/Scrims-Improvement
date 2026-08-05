0fba38a8a8ef6f3eb210f3c7e070e910b718d463
SoloSecondBest <jokubas.saudargas2020@gmail.com>
2026-08-05 22:41:18 +0300

    Add files via upload

---
+++ mapsData.js
@@ -0,0 +1,116 @@
+const maps = [
+
+{
+name:"Dry Season",
+mode:"Bounty",
+image:"images/dry_season.png"
+},
+
+{
+name:"Hideout",
+mode:"Bounty",
+image:"images/hideout.png"
+},
+
+{
+name:"Layer Cake",
+mode:"Bounty",
+image:"images/layer_cake.png"
+},
+
+
+{
+name:"Pit Stop",
+mode:"Heist",
+image:"images/pit_stop.png"
+},
+
+{
+name:"Safe Zone",
+mode:"Heist",
+image:"images/safe_zone.png"
+},
+
+{
+name:"Kaboom Canyon",
+mode:"Heist",
+image:"images/kaboom_canyon.png"
+},
+
+
+{
+name:"Ring of Fire",
+mode:"Hot Zone",
+image:"images/ring_of_fire.png"
+},
+
+{
+name:"Open Business",
+mode:"Hot Zone",
+image:"images/open_business.png"
+},
+
+{
+name:"Dueling Beetles",
+mode:"Hot Zone",
+image:"images/dueling_beetles.png"
+},
+
+
+{
+name:"Triple Dribble",
+mode:"Brawl Ball",
+image:"images/triple_dribble.png"
+},
+
+{
+name:"Pinhole Punt",
+mode:"Brawl Ball",
+image:"images/pinhole_punt.png"
+},
+
+{
+name:"Pinball Dreams",
+mode:"Brawl Ball",
+image:"images/pinball_dreams.png"
+},
+
+
+{
+name:"Hard Rock Mine",
+mode:"Gem Grab",
+image:"images/hard_rock_mine.png"
+},
+
+{
+name:"Gem Fort",
+mode:"Gem Grab",
+image:"images/gem_fort.png"
+},
+
+{
+name:"Crystal Arcade",
+mode:"Gem Grab",
+image:"images/crystal_arcade.png"
+},
+
+
+{
+name:"Goldarm Gulch",
+mode:"Knockout",
+image:"images/goldarm_gulch.png"
+},
+
+{
+name:"New Horizons",
+mode:"Knockout",
+image:"images/new_horizons.png"
+},
+
+{
+name:"Out in the Open",
+mode:"Knockout",
+image:"images/out_in_the_open.png"
+}
+
+];
