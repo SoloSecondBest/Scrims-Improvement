3a5cf8360822f57859ef3f1e75c37c7c5d1ee6b4
SoloSecondBest <jokubas.saudargas2020@gmail.com>
2026-08-05 08:38:29 +0300

    Add files via upload

---
+++ brawlers.js
@@ -0,0 +1,22 @@
+const brawlers = {
+
+    Rare: [
+
+        {
+            name:"Shelly",
+            image:"rare/shelly.png"
+        },
+
+        {
+            name:"Nita",
+            image:"rare/nita.png"
+        },
+
+        {
+            name:"Colt",
+            image:"rare/colt.png"
+        }
+
+    ]
+
+};
