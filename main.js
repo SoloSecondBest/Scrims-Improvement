469330e72d3e538b64265e01d2f296060cd3f46a
SoloSecondBest <jokubas.saudargas2020@gmail.com>
2026-08-05 09:06:16 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+function openPage(page, button){
+
+
+    let buttons = document.querySelectorAll(".nav-button");
+
+
+    buttons.forEach(btn => {
+        btn.classList.remove("active");
+    });
+
+
+    button.classList.add("active");
+
+
+
+    let content = document.getElementById("content");
+
+
+
+    if(page === "brawlers"){
+
+        loadBrawlers();
+
+    }
+
+
+    if(page === "maps"){
+    
+      content.innerHTML = mapsPage;
+
+    }
+
+
+    if(page === "players"){
+
+        content.innerHTML = `
+
+        <h1>Players</h1>
+
+        `;
+
+    }
+
+
+    if(page === "drafts"){
+
+        content.innerHTML = `
+
+        <h1>Drafts</h1>
+
+        `;
+
+    }
+
+}
